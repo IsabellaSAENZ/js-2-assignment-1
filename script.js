a955439a5807f969a880e677e62ada843ee81016
Isabella Saenz <saenzisabella448@gmail.com>
2023-12-09 12:28:01 -0600

    added elms added fors and ifs

--- script.js
+++ script.js
@@ -14,21 +14,44 @@ elms = document.querySelectorAll("h1");
 // Loop through the elements and change them.
 for(elm of elms) {
     elm.style.color = "red";
+    elm.style.backgroundColor ="purple";
     // TODO: set the backgroundColor;
 }
 
 // TODO: select and change the p tags
 //       then loop through the elements and change them.
-
+pElms = document.querySelectorAll("p");
+ for (elm of pElms) {
+    elm.style.fontSize = "55px";
+    elm.style.backgroundColor ="purple";   
+}
 
 // TODO: select and change the span tags
 //       then loop through the elements and change them.
-
+sElms = document.querySelectorAll("span");
+for (elm of sElms) {
+    elm.style.color = "blue";
+}
 
 // TODO: Go back to index.html and add
 //       a class called "jump" to 10 elements.
 //       These can be any elements you want.
 //       For example: 2 h1 tags, 3 p tags, 5 span tags.
+elms= document.querySelectorAll(".jump");
+
+function oneJump(event) {
+    elm = event.target;
+    elm.style.position ="relative";
+    elm.style.transition ="transform 2s"
+    elm.style.transform ="translateX(-100px)";
+    elm.style.color= "green";
+    elm.style.backgroundColor = "yellow";
+
+}
+
+for (jumper of elms) {
+    jumper.onclick = oneJump;
+}
 
 
 // TODO: select and change the elements with a .jump class 
